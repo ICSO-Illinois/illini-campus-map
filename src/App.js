020a1a9263da683a97735d44cfd451b97a634226
danielz02 <chenhui5@illinois.edu>
2020-04-12 02:36:26 -0500

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 import Leaflet from 'leaflet'
 import React, { StrictMode } from 'react'
 import './App.css'
-import CampusMap from './campus'
 import SearchAppBar from "./appbar"
 
 Leaflet.Icon.Default.imagePath = '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/';
@@ -11,8 +10,6 @@ Leaflet.Icon.Default.imagePath = '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4
 const App = () => (
     <StrictMode>
         <div><SearchAppBar/></div>
-        <h1>Illini Campus Map</h1>
-        <h2><CampusMap/></h2>
     </StrictMode>
 );
 
