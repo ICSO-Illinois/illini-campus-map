f4b1ecf3c14a27f5b2e550fc0ff5533997b5d813
danielz02 <chenhui5@illinois.edu>
2020-04-12 04:14:41 -0500

    Modify dummy test case (still not working)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {test} from "pretty-format";
 
 test('renders learn react link', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/Campus Map/i);
-    expect(linkElement).toBeInTheDocument();
 });
