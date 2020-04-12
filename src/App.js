a997cc6040055bc3683800b3b106492e9bb09a4c
danielz02 <chenhui5@illinois.edu>
2020-04-12 05:22:25 -0500

    Enhance mobile compatibility

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import Leaflet from 'leaflet'
 import React, { StrictMode } from 'react'
 import './App.css'
 import SearchAppBar from "./appbar"
+import Container from '@material-ui/core/Container';
 import CampusMap from "./campus";
+import Typography from "@material-ui/core/Typography";
 
 Leaflet.Icon.Default.imagePath = '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/';
 
 const App = () => (
     <StrictMode>
-        {SearchAppBar(
-            [
-                <h1>Illini Campus Map</h1>,
-                <CampusMap/>
-                ])
-        }
+        {SearchAppBar([])}
+        <CampusMap/>
     </StrictMode>
 );
 
--- src/appbar.js
+++ src/appbar.js
@@ -61,7 +61,7 @@ const useStyles = makeStyles((theme) => ({
     drawerHeader: {
         display: 'flex',
         alignItems: 'center',
-        padding: theme.spacing(0, 1),
+        // padding: theme.spacing(0, 1),
         // necessary for content to be below app bar
         ...theme.mixins.toolbar,
         justifyContent: 'flex-end',
@@ -145,7 +145,7 @@ export default function SearchAppBar(mainComponents) {
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
+            <AppBar position="fixed">
                 <Toolbar>
                     <IconButton
                         color="inherit"
