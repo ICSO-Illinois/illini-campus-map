ab09c58cb7d533327ef3cc8e8e3a847ec419aadf
danielz02 <chenhui5@illinois.edu>
2020-04-12 01:52:39 -0500

    Add App Bar with dummy search field

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@
 
 import Leaflet from 'leaflet'
 import React, { StrictMode } from 'react'
-
+import './App.css'
 import CampusMap from './campus'
+import SearchAppBar from "./appbar"
 
-Leaflet.Icon.Default.imagePath =
-    '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/';
+Leaflet.Icon.Default.imagePath = '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/';
 
 const App = () => (
     <StrictMode>
+        <div><SearchAppBar/></div>
         <h1>Illini Campus Map</h1>
-        <CampusMap />
+        <h2><CampusMap/></h2>
     </StrictMode>
 );
 
